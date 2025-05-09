a4b40c65201bc1c15fc75e81984f3233d37d2068
unknown <abdealidahodwala163@gmail.com>
2025-05-09 14:09:04 +0530

    FEAT : Hero section created

---
+++ src/components/Hero.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./MagicButton";
+import { FaDownload, FaLocationArrow } from "react-icons/fa6";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="pb-20 pt-36">
+        <div>
+          <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="white"
+          />
+          <Spotlight
+            className="h-[80vh] w-[50vw] top-10 left-full"
+            fill="purple"
+          />
+          <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+        </div>
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <TextGenerateEffect
+            words="Its Abdeali,"
+            className="text-center text-amber-50 text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            A passionate web developer on a journey of becoming a full stack
+            developer.
+          </p>
+          <div className="flex gap-7">
+            <a href="#about">
+              <MagicButton
+                title="My works"
+                icon={<FaLocationArrow />}
+                position="right"
+              />
+            </a>
+
+            <a href="#about">
+              <MagicButton
+                title="Resume"
+                icon={<FaDownload />}
+                position="right"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
